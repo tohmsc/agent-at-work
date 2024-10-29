f58cd674f27b950aae729c41a625672b98c97420
TMS <127390442+tohmsc@users.noreply.github.com>
2024-10-29 20:28:53 +0100

    add supabase auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,14 @@
-import { type NextRequest } from "next/server";
-import { updateSession } from "@/utils/supabase/middleware";
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient({ req, res })
+
+  await supabase.auth.getSession()
+
+  return res
 }
 
 export const config = {
