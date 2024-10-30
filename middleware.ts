7638fe56fdf79c66f49c7650ff54781b0a82271b
TMS <127390442+tohmsc@users.noreply.github.com>
2024-10-30 09:00:02 +0100

    3 fix: update middleware for production deployment

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)'],
-  runtime: 'nodejs'
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)']
 };
 
 const securityHeaders = {
